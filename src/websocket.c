706ce93f31d6ccafe1b5cf59c2d762be433ccdf2
kavinvin <59070009@it.kmitl.ac.th>
2017-05-06 19:12:03 +0700

    Fix header copying segmentation fault

--- src/websocket.c
+++ src/websocket.c
@@ -53,7 +53,7 @@ int openHandshake(User *user) {
         printlog("Didn't receive any message from client");
     }
 
-    header->string = calloc(length+1, 1);
+    header->string = calloc(length+1, sizeof(char));
     if (header->string == NULL) {
         printlog("Memory allocation failed: %s\n", strerror(errno));
         return -1;
